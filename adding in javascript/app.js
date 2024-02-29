fce4de430b7b5b623405d58d18e020f16f5e44a3
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-02-29 11:30:26 +0500

    Add files via upload

---
+++ adding in javascript/app.js
@@ -0,0 +1,4 @@
+const num1 = parseInt(prompt('Enter Your First Number to Add'));
+const num2 = parseInt(prompt('Enter Your Second Number to Add '));
+const sum = num1 + num2;
+document.write(`Your Sum is ${sum}`); 
